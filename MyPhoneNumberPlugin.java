08d54976cc8103b7964ea102a52012d6df6e4535
Guy Vider <guy@guyvider.com>
2011-08-29 19:26:09 -0700

    Added support for non-phone devices by using a hash of the device's ID

--- MyPhoneNumberPlugin.java
+++ MyPhoneNumberPlugin.java
@@ -1,4 +1,10 @@
-package com.travelingtechguy.myphonenumberplugin;
+/**
+ * 
+ */
+package com.TravelingTechGuy.ImAlive;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -13,17 +19,11 @@ import com.phonegap.api.PluginResult;
 import com.phonegap.api.PluginResult.Status;
 
 /**
- * @author Guy Vider, © 2011 Traveling Tech Guy LLC
+ * @author Guy Vider
  *
  */
 public class MyPhoneNumberPlugin extends Plugin {
 
-	/***
-	 * @param action - name of action to execute (if more than 1 available)
-	 * @param data - JSON data input
-	 * @param callbackId - function to call on completion
-	 * @return PluginResult containing result data
-	 */
 	@Override
 	public PluginResult execute(String action, JSONArray data, String callbackId) {
 		Log.d("MyPhoneNumberPlugin", "Plugin called");
@@ -39,20 +39,38 @@ public class MyPhoneNumberPlugin extends Plugin {
 		}
 		return result;
 	}
-	
-	/***
-	 * 
-	 * @return JSONObject containing phone number
-	 * @throws JSONException
-	 */
+
 	private JSONObject getMyPhoneNumber() throws JSONException {
 		Log.d("MyPhoneNumberPlugin", "at getMyPhoneNumber");
 		JSONObject result = new JSONObject();
 		TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
         String number = tm.getLine1Number();
+        number = "";
+        if(number.equals("") || number == null) {
+        	Log.d("MyPhoneNumberPlugin", "We're on a non-phone device. Returning a hash of the UDID");
+        	number = md5(tm.getDeviceId()).substring(0, 10);
+        }
         Log.d("MyPhoneNumberPlugin", "Phone number=" + number);
         result.put("phoneNumber", number);
 		return result;
 	}
+	
+	private String md5(String s) {
+	    try {
+	        // Create MD5 Hash
+	        MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+	        digest.update(s.getBytes());
+	        byte messageDigest[] = digest.digest();
+	        
+	        // Create Hex String
+	        StringBuffer hexString = new StringBuffer();
+	        for (int i=0; i<messageDigest.length; i++)
+	            hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+	        return hexString.toString();
+	        
+	    } catch (NoSuchAlgorithmException e) {
+	    	Log.d("MyPhoneNumberPlugin", e.getMessage());
+	    }
+	    return "";
+	}
 }
-
